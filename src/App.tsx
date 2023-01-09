be6898ae04b2aba1f7e32409539a1028db408c9b
2017Yasu <yasu.nkym.96@gmail.com>
2023-01-09 10:25:04 +0900

    sassを導入

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import FunctionCallbackComponent from './components/function-callback-component'
 import Clock from './components/class-lifecycle-component';
 import LiftingStateUpComponent from './components/lifting-state-up-component';
 
-import './App.css';
+import './App.scss';
 
 function App(): JSX.Element {
     return (
